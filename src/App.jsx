a24d207d999cc225c246e5550604995a98a29195
Layljan <ljdumayas@gmail.com>
2024-07-19 20:03:41 +0800

    Creating design

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,31 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div>
-      <h1>Hello World</h1>
+      <div className="head"></div>
+      <div className="content">
+        <table>
+          <tr>
+            <th>Item</th>
+            <th>Quantity</th>
+            <th>Description</th>
+            <th>Action</th>
+          </tr>
+          <tr>
+            <td>Coal</td>
+            <td>4</td>
+            <td>Its coal it cooks stuff</td>
+            <td className="actions">
+              <button className="action">Edit</button>
+              <button className="action">Delete</button>
+            </td>
+          </tr>
+        </table>
+        <div className="addbtn">
+          <button>add more items</button>
+        </div>
+      </div>
     </div>
   );
 }
